c643cb75408ec5fc2acd290f1fc127081aafd41b
ldj5123 <ldj5123@naver.com>
2020-07-13 17:33:58 +0900

    Reference counting

---
+++ string2/string.h
@@ -0,0 +1,34 @@
+#ifndef STRING_H
+#define STRING_H
+
+#include <iostream>
+#include "stringRep.h"
+
+
+class String{
+friend std::ostream& operator<<(std::ostream& out, const String& rhs);
+private:
+//	char *str;
+//	int len;
+	StringRep *ptr_;
+	
+	String(const char *str, bool);
+	
+public:
+//	String();
+	String(const char *str = NULL);
+	String(const String& rhs);
+	~String();
+	
+	String& operator=(const String& rhs);	// daisy-chain이 가능하기 위해서는 반환값이 String&이 되어야한다.
+	String& operator=(const char *str);
+	
+	bool operator==(const String& rhs) const;
+	
+	const String operator+(const String& rhs) const;
+	
+	const char *c_str() const;
+	int length() const;
+};
+
+#endif
