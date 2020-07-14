9d136126e0c2a29b6a3999d6a4f1f391de3be9ab
ldj5123 <ldj5123@naver.com>
2020-07-14 18:09:47 +0900

    template

---
+++ C++/array2/main.cpp
@@ -0,0 +1,20 @@
+#include "array.h"
+#include <iostream>
+
+int main()
+{
+	int nums1[] = {1, 2, 3, 4, 5};
+	Array<int> arr1(nums1, 5);		// 꺽쇠를 이용해 자료형을 준다.
+	for (int i = 0; i < arr1.size(); ++i)
+		std::cout << arr1[i] << " ";
+	std::cout << std::endl;
+	
+	
+	
+	double nums2[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+	Array<double> arr2(nums2, 5);
+	for (int i = 0; i < arr2.size(); ++i)
+		std::cout << arr2[i] << " ";
+	std::cout << std::endl;
+	return 0;
+}
